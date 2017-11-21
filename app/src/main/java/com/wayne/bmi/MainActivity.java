e603802301aaeb8d4a36f3e6ccbf2c8f9cd06610
WayneKuo <aasdfangela@gmail.com>
2017-11-21 12:02:03 +0800

    Intent

--- app/src/main/java/com/wayne/bmi/MainActivity.java
+++ app/src/main/java/com/wayne/bmi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wayne.bmi;
 
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,19 +11,23 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-   // String s = new String("abc");
+    private EditText edweight;
+    private EditText edheight;
+    private Button bHelp;
+    // String s = new String("abc");
    // View.OnClickListener listener = new View.OnClickListener() {
    //    @Override
     //   public void onClick(View view) {
 
     //   }
   // };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      //  getResources().getString(R.string.app_name);
-      /*  Button bHelp = (Button)findViewById(R.id.b_help);
+        getResources().getString(R.string.app_name);
+      //  bHelp = (Button)findViewById(R.id.b_help);
         bHelp.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -33,32 +38,44 @@ public class MainActivity extends AppCompatActivity {
                         .show();
             }
         });
-        */
+
     }
     public void bmi(View view){
     //    System.out.println("123");
         Log.d("MainActivity","testing bmi method ");
-        EditText edweight = (EditText) findViewById(R.id.ed_weight);
-        EditText edheight = (EditText) findViewById(R.id.ed_height);
+        findView();
         float weight = Float.parseFloat(edweight.getText().toString());
         float height = Float.parseFloat(edheight.getText().toString());
         float bmi = weight / (height*height);
+
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("BMI_EXTRA", bmi);
+        startActivity(intent);
+
+        Log.d("MainActivity","onCreate");
+
         /*Log.d("MainActivity","Your bmi is:"+bmi);
         Toast.makeText(this,"Your bmi is" + bmi, Toast.LENGTH_LONG).show();*/
 
-         new AlertDialog.Builder(this)
+        /* new AlertDialog.Builder(this)
                     .setMessage(getString(R.string.your_bmi_is) + bmi)
                     .setTitle(R.string.my_title)
                     .setPositiveButton(R.string.OK, null)
-                    .show();
+                    .show();*/
        /* if(bmi<=20){
             Toast.makeText(this,"請多吃點",Toast.LENGTH_LONG).show();
         }*/
-        if(R.id.ed_height>3){
+       /* if(R.id.ed_height>3){
             new AlertDialog.Builder(this)
                     .setMessage("身高單位應為公尺")
                     .setPositiveButton("OK",null)
                     .show();
-        }
+        }*/
+    }
+
+    private void findView() {
+        edweight = (EditText) findViewById(R.id.ed_weight);
+        edheight = (EditText) findViewById(R.id.ed_height);
+        bHelp = (Button)findViewById(R.id.b_help);
     }
 }
